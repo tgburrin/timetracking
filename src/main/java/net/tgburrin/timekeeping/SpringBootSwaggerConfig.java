7232cc361b22a40597298ec77464b24075063032
Tim Burrington <tgburrin>
2024-09-09 21:19:04 -0400

    Trying to figure out the swagger frontend in spring boot v3.

--- src/main/java/net/tgburrin/timekeeping/SpringBootSwaggerConfig.java
+++ src/main/java/net/tgburrin/timekeeping/SpringBootSwaggerConfig.java
@@ -3,19 +3,23 @@ package net.tgburrin.timekeeping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@OpenAPIDefinition()
 public class SpringBootSwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI swaggerApiConfig() {
+        var info = new Info()
+                .title("Heroes API")
+                .description("Spring Boot Project to demonstrate SwaggerUI integration")
+                .version("1.0");
+        return new OpenAPI().info(info);
     }
 }
