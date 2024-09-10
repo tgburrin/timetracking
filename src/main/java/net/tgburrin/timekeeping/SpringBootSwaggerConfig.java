f691fee116fe93cdb602f88ed54644f95f31a625
Tim Burrington <tgburrin>
2024-09-10 08:53:33 -0400

    Attempting to fix up openapi and removing springfox since it is no longer supported

--- src/main/java/net/tgburrin/timekeeping/SpringBootSwaggerConfig.java
+++ src/main/java/net/tgburrin/timekeeping/SpringBootSwaggerConfig.java
@@ -1,25 +1,26 @@
 package net.tgburrin.timekeeping;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
 @Configuration
-@OpenAPIDefinition()
 public class SpringBootSwaggerConfig {
     @Bean
-    public OpenAPI swaggerApiConfig() {
-        var info = new Info()
-                .title("Heroes API")
-                .description("Spring Boot Project to demonstrate SwaggerUI integration")
-                .version("1.0");
-        return new OpenAPI().info(info);
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("springshop-public")
+                .pathsToMatch("/public/**")
+                .build();
     }
+    /*
+    @Bean
+    public GroupedOpenApi adminApi() {
+        return GroupedOpenApi.builder()
+                .group("springshop-admin")
+                .pathsToMatch("/admin/**")
+                .addOpenApiMethodFilter(method -> method.isAnnotationPresent(Admin.class))
+                .build();
+    }
+     */
 }
--- src/main/java/net/tgburrin/timekeeping/TimekeepingApplication.java
+++ src/main/java/net/tgburrin/timekeeping/TimekeepingApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -16,7 +17,6 @@ import java.util.Date;
 import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableJdbcRepositories
 public class TimekeepingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
